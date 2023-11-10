e56e03e22f521943196d5501750dbb7da790a04a
Estebang2001 <110405527+Estebang2001@users.noreply.github.com>
2023-11-10 17:27:48 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+/* Ejercicio Práctico
+En este ejercicio deberás convertir las siguientes Funciones a Arrow Functions:*/
+
+// 1er Ejercicio
+
+/*function sumarLosTres(num1, num2, num3) {
+ return num1 + num2 + num3
+} */
+
+// SOLUCION 1er Ejercicio
+
+let sumarLosTres = (num1,num2,num3) => {
+    return num1 + num2 + num3
+} 
+
+// 2do Ejercicio
+
+/* function cuadrado(num) {
+    return num * num
+   } */
+
+// SOLUCION 2do Ejercicio
+
+let cuadrado = (num) => num * num
+
+// 3er Ejercicio
+
+/* function decirHola() {
+ console.log('¡Hola!');
+} */
+
+// SOLUCION 3er Ejercicio 
+
+let decirHola = () => console.log("Hola!")
